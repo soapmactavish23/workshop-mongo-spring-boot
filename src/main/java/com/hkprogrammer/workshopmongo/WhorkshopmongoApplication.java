64aef64eba68466ffb5a75fc1c3f5652ec4dd08d
unknown <ricknogueira1231@gmail.com>
2023-01-09 16:40:34 -0300

    first commit

---
+++ src/main/java/com/hkprogrammer/workshopmongo/WhorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.hkprogrammer.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hkprogrammer/workshopmongo/WhorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkprogrammer.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
